3503f6f18a0f4669e62236b1473ff49eafb8f1d8
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-11 15:34:14 +0200

    :white_check_mark: Add tests for product model

--- src/models/tests/productSpec.ts
+++ src/models/tests/productSpec.ts
@@ -14,4 +14,30 @@ describe("Product Model", () => {
   it('should have a create method', () => {
     expect(store.create).toBeDefined();
   });
+
+  it('should create a product', async () => {
+    const result = await store.create({
+      name: 'Jam',
+      price: 20
+    });
+    expect(result).toEqual({
+      id: 1,
+      name: 'Jam',
+      price: 20
+    });
+  });
+
+  it('should show a Product', async () => {
+    const result = await store.show("1");
+    expect(result).toEqual({
+      id: 1,
+      name: 'Jam',
+      price: 20
+    });
+  })
+
+  it('should show a list of Products', async () => {
+    const result = await store.index();
+    expect(result).toBeInstanceOf(Array<Product>);
+  })
 });
