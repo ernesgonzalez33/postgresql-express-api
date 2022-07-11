0fb225f13e4e37162c2bbebecfa4460bb871a5dd
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-11 12:54:59 +0200

    :white_check_mark: Add test for order model

--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,36 @@
-import dotenv from 'dotenv';
-import { Pool } from 'pg';
+import dotenv from "dotenv";
+import { Client, Pool } from "pg";
 
-dotenv.config()
+dotenv.config();
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-} = process.env 
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
 
-const client = new Pool({
+let client: Pool;
+
+if (ENV === "test") {
+  client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
-})
+  });
+} else if (ENV === "dev") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+} else {
+    client = new Pool(undefined);
+    console.error("That is not a correct environment");
+    process.exitCode = 1;
+}
 
-export default client
+export default client;
---
+++ src/models/tests/orderSpec.ts
@@ -0,0 +1,9 @@
+import { Order, OrderStore } from "../order";
+
+const store = new OrderStore();
+
+describe("Order Model", () => {
+    it('should have a Current Order by User method', () => {
+        expect(store.currentOrderByUser).toBeDefined();
+    })
+})
