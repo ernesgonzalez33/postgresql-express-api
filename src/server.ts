dc66ba188a17b7b04da053198ca525fdbc11db85
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-13 09:41:27 +0200

    :bug: Add routes in the server file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import productRoutes from './handlers/product'
+import userRoutes from './handlers/user'
+import orderRoutes from './handlers/order'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -10,6 +13,13 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
+productRoutes(app);
+userRoutes(app);
+orderRoutes(app);
+
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
 })
+
+
+export default app;
