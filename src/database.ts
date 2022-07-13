7148165b8bbef134623431741afbb614ea8599dd
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-13 19:48:56 +0200

    :bug: Database.ts not pointing to correct database

--- src/database.ts
+++ src/database.ts
@@ -8,7 +8,7 @@ const {
   POSTGRES_TEST_DB,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
-  ENV,
+  ENV
 } = process.env;
 
 let client: Pool;
@@ -16,14 +16,14 @@ let client: Pool;
 if (ENV === "test") {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_DB,
+    database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   });
 } else if (ENV === "dev") {
   client = new Pool({
     host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
+    database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
   });
