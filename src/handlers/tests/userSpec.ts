96f00d276a1aa7f7d3071d168b8bb6f9af46f8ec
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-13 11:10:16 +0200

    :white_check_mark: Add test for users endpoints

---
+++ src/handlers/tests/userSpec.ts
@@ -0,0 +1,36 @@
+import supertest from "supertest";
+import app from "../../server";
+
+const req = supertest(app);
+
+const createTestUser = async () => {
+  const res = await req.post("/users").send({
+    firstName: "Test",
+    lastName: "User",
+    password: "pass123",
+  });
+  return res;
+};
+
+describe("User Handler", () => {
+  it("should have created a new user", async () => {
+    const res = await createTestUser();
+    expect(res.status).toBe(200);
+  });
+
+  it("should return a 200 status in the index endpoint", async () => {
+    const token = await createTestUser();
+    const res = await req
+      .get("/users")
+      .set("Authorization", "Bearer " + token.body);
+    expect(res.status).toBe(200);
+  });
+
+  it("should return a 200 status in the show endpoint", async () => {
+    const token = await createTestUser();
+    const res = await req
+      .get("/users/1")
+      .set("Authorization", "Bearer " + token.body);
+    expect(res.status).toBe(200);
+  });
+});
