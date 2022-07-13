a69c71ba92f036f62cc1a6f1c693b5412e854382
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2022-07-13 10:31:15 +0200

    :art: Prettify product handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { Product, ProductStore } from "../models/product";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const store = new ProductStore();
 
@@ -15,28 +15,28 @@ const show = async (req: Request, res: Response) => {
 };
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const authorizationHeader = req.headers.authorization
-        const token = authorizationHeader!.split(' ')[1]
-        jwt.verify(token, process.env.TOKEN_SECRET as string)
-    } catch(err) {
-        res.status(401)
-        res.json('Access denied, invalid token')
-        return
-    }
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader!.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
+  } catch (err) {
+    res.status(401);
+    res.json("Access denied, invalid token");
+    return;
+  }
 
-    try {
-        const product: Product = {
-            name: req.body.name,
-            price: req.body.price
-        }
+  try {
+    const product: Product = {
+      name: req.body.name,
+      price: req.body.price,
+    };
 
-        const newProduct = await store.create(product)
-        res.json(newProduct)
-    } catch(err) {
-        res.status(400)
-        res.json(err)
-    }
+    const newProduct = await store.create(product);
+    res.json(newProduct);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
 const productRoutes = (app: express.Application) => {
